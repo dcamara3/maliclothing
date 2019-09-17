84c5b1cddad263579642e25d1b06ec0d550f5aa8
dcamara3 <dcamara3@gmail.com>
2019-09-17 19:51:19 -0400

    further renaming

--- maliclothing-ui/e2e/src/app.e2e-spec.ts
+++ maliclothing-ui/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('horon-ui app is running!');
+    expect(page.getTitleText()).toEqual('maliclothing-ui app is running!');
   });
 
   afterEach(async () => {
