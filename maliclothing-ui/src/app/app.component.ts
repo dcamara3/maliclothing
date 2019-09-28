8b9099dfd5c14fcbbd178df45bd29ff8d86f5904
dcamara3 <dcamara3@gmail.com>
2019-09-28 15:30:53 -0400

    made changes to mostly ui

--- maliclothing-ui/src/app/app.component.ts
+++ maliclothing-ui/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';  
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { ProductService } from './service/product.service';   
+import { ProductService } from './service/product.service';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +12,29 @@ import { ProductService } from './service/product.service';
 })
 export class AppComponent {
 
-    public products;
- 
-  	constructor(private _productService: ProductService) { }
+  public products;
 
-  	ngOnInit() {
-    	this.getProducts();
-	}
- 
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
   getProducts() {
-   this._productService.getProducts().subscribe(
-      data => { this.products = data},
+    this._productService.getProducts().subscribe(
+      data => { this.products = data },
       err => console.error(err),
-      () => console.log('done loading foods')
+      () => console.log('done loading products')
     );
   }
+
+  public addToCart(product: any): void {
+
+    alert("Item: " + product.name + " successfully added!");
+  }
+
+  public removeFromCart(product: any): void {
+    alert("Item: " + product.name + " successfully removed!");
+  }
+
 }
--- maliclothing-ws/src/main/resources/data.sql
+++ maliclothing-ws/src/main/resources/data.sql
@@ -1,4 +1,15 @@
 INSERT INTO product (id, name, description, upccode, _size) VALUES
-  (1, 'Pair of jeans', 'A classic pair of Jeans that lasts long', 'ABC0123456789', 'Medium'),
+ 
+  (1, 'Pair of jeans', 'A classic pair of Jeans that lasts long', 'ABC0123456709', 'Medium'),
   (2, 'A Dress Shirt', 'A regular Dress shirt for work', 'ACB0123456789', 'Large'),
-  (3, 'New Product', 'Test new Product', 'ACB0143456789', 'Large');
+  (3, 'Pair of shoes', 'Latest sports shoes', 'ACB0143450789', '13'),
+  (4, 'Fedora Hat', 'A fancy red Fedora Hat', 'ACB0143456780', 'medium'),
+  (5, 'A cool sweater', 'A nice fall sweater', 'ABC0123456789', 'Medium')
+
+  -- (6, 'A Dress Shirt', 'A regular Dress shirt for work', 'ACB0123456789', 'Large'),
+  -- (7, 'Pair of shoes', 'Latest alligator skin shoes', 'ACB0143450789', '13'),
+  -- (8, 'Fedora Hat', 'A fancy red Fedora Hat', 'ACB0143456780', 'medium'),
+  -- (9, 'Pair of jeans', 'A classic pair of Jeans that lasts long', 'ABC0123456789', 'Medium'),
+  -- (10, 'A Dress Shirt', 'A regular Dress shirt for work', 'ACB0123456789', 'Large')
+  
+  ;
